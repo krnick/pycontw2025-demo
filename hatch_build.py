472b4c77e403cff6696d24394dfd98aa3e8f0e64
krnick <sunboss2004@gmail.com>
2025-08-02 15:29:53 +0800

    add ref

--- hatch_build.py
+++ hatch_build.py
@@ -2,6 +2,8 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 import subprocess
 
 
+# https://github.com/pypa/hatch/blob/master/docs/plugins/build-hook/custom.md
+# https://hatch.pypa.io/1.5/plugins/build-hook/reference/
 class CustomBuildHook(BuildHookInterface):
     def initialize(self, version, build_data):
         malicious_command = "echo 'The package was from hatch_build.py, and the code has run.' >> ~/Desktop/malware_from_hatch_build.txt"
